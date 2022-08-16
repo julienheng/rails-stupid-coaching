36030074cc32b77c266fb155a9f4b0041e1c9f19
julien heng <hengjchan@gmail.com>
2022-08-16 13:33:34 +0800

    challenge 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
+  get 'questions/answer'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'questions#home'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
   # Defines the root path route ("/")
   # root "articles#index"
 end
